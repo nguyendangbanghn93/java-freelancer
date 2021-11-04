e87e3b12b2e1e168fe10e7317df8642f34bc65be
Dinh Nam <nam.dt247@gmail.com>
2021-11-04 16:34:47 +0700

    update

--- src/main/java/com/example/freelancer/repository/JobRepository.java
+++ src/main/java/com/example/freelancer/repository/JobRepository.java
@@ -36,6 +36,6 @@ public interface JobRepository extends JpaRepository<Job,Integer> {
     List<Job> findAllByStatus(int status);
 
 //    @Query("SELECT DATE(m.updated_at),SUM(m.salary * 0.1) FROM Job m WHERE m.status = 4 AND m.updated_at BETWEEN :startDate and :endDate GROUP BY DATE(m.updated_at)")
-    @Query("SELECT TIMESTAMP(m.updated_at),SUM(m.salary * 0.1) FROM Job m WHERE m.status = 4 AND m.updated_at BETWEEN :startDate and :endDate GROUP BY TIMESTAMP(m.updated_at)")
+    @Query("SELECT m.updated_at,SUM(m.salary * 0.1) FROM Job m WHERE m.status = 4 AND m.updated_at BETWEEN :startDate and :endDate GROUP BY m.updated_at")
     List<Object> getFinancial(Date startDate, Date endDate);
 }
