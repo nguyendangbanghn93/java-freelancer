2ea5f2d9b59232166ca8354ba533368bad66bee8
Dinh Nam <nam.dt247@gmail.com>
2021-11-08 12:12:25 +0700

    update list transaction

--- src/main/java/com/example/freelancer/repository/TransactionHistoryRepository.java
+++ src/main/java/com/example/freelancer/repository/TransactionHistoryRepository.java
@@ -1,6 +1,8 @@
 package com.example.freelancer.repository;
 
 import com.example.freelancer.entity.TransactionHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +15,7 @@ public interface TransactionHistoryRepository extends JpaRepository<TransactionH
 
     @Query("SELECT m.createdAt,SUM(m.amount) FROM TransactionHistory m WHERE m.createdAt BETWEEN :startDate and :endDate GROUP BY m.createdAt")
     List<Object[]> getTransactionHistoryByCreatedAtBetween(Date startDate, Date endDate);
+
+    @Query("SELECT m FROM TransactionHistory m ORDER BY m.createdAt DESC")
+    Page<TransactionHistory> findAll(Pageable pageable);
 }
